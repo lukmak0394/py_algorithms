7353d5742be8b46e82922c3d4d3d8eaa20db5502
lukmak0394 <=>
2024-08-24 08:58:13 +0200

    ll methods

---
+++ linked_lists/LinkedList.py
@@ -0,0 +1,30 @@
+from Node import Node
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value) -> bool:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(5)
+my_linked_list.print_list()
